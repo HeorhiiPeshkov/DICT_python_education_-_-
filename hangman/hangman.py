9f6e45196b2aadd92f8ab86ab9063ef0e2c9713f
HeorhiiPeshkov <h.p.pieshkov@student.khai.edu>
2024-10-22 15:29:02 +0300

    Hangman 4-th stage

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -9,25 +9,25 @@
 # else:
 #     print("You lost!")
 
-#stage 3
-import random
-programming_languages = ["python", 'java', 'javascript', 'php', 'pascal', 'c#', 'c++']
-ran_prog_lang = (random.choice(programming_languages))
-writ_ran_prog_lang = input("Guess the word:>")
-if writ_ran_prog_lang == ran_prog_lang:
-    print('You survived!')
-else:
-    print('You lost!')
-
-# #stage 4
+# #stage 3
 # import random
-# programming_languages = ["python", 'java', 'javascript', 'php', 'pascal', 'c#']
-# hid_prog_lang = ["pyt---", 'j---', 'jav-------', '---', 'pas---', '--']
+# programming_languages = ["python", 'java', 'javascript', 'php', 'pascal', 'c#', 'c++']
 # ran_prog_lang = (random.choice(programming_languages))
-# ran_hid_prog_lang = (random.choice(hid_prog_lang))
-# writ_ran_prog_lang = input(f"Guess the word {ran_hid_prog_lang}:>")
+# writ_ran_prog_lang = input("Guess the word:>")
 # if writ_ran_prog_lang == ran_prog_lang:
 #     print('You survived!')
 # else:
 #     print('You lost!')
 
+# # #stage 4
+import random
+programming_languages = ["python", "java", "pascal", 'golang', 'assembler']
+hid_prog_lang = [lang[:3] + "-" * (len(programming_languages) - 3) for lang in programming_languages]
+ran_index = random.randrange(len(programming_languages))
+ran_prog_lang = programming_languages[ran_index]
+ran_hid_prog_lang = hid_prog_lang[ran_index]
+writ_ran_prog_lang = input(f"Guess the word {ran_hid_prog_lang}:> ")
+if writ_ran_prog_lang == ran_prog_lang:
+    print('You survived!')
+else:
+    print('You lost!')
