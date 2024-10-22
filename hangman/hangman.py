ae81dfba9ae7567067c7fc09f6b6bc4c337ae92d
HeorhiiPeshkov <h.p.pieshkov@student.khai.edu>
2024-10-22 13:31:56 +0300

    Hangman 3-rd stage

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,23 +1,23 @@
 # #stage 1
 # print("HANGMAN.\nThe game will be available soon.")
 
-#stage 2
-print("HANGMAN")
-word = input("Guess the word:>")
-if word == "python":
-    print("You survived!")
-else:
-    print("You lost!")
-
-# #stage 3
-# import random
-# programming_languages = ["python", 'java', 'javascript', 'php', 'pascal', 'c#', 'c++']
-# ran_prog_lang = (random.choice(programming_languages))
-# writ_ran_prog_lang = input("Guess the word:>")
-# if writ_ran_prog_lang == ran_prog_lang:
-#     print('You survived!')
+# # stage 2
+# print("HANGMAN")
+# word = input("Guess the word:>")
+# if word == "python":
+#     print("You survived!")
 # else:
-#     print('You lost!')
+#     print("You lost!")
+
+#stage 3
+import random
+programming_languages = ["python", 'java', 'javascript', 'php', 'pascal', 'c#', 'c++']
+ran_prog_lang = (random.choice(programming_languages))
+writ_ran_prog_lang = input("Guess the word:>")
+if writ_ran_prog_lang == ran_prog_lang:
+    print('You survived!')
+else:
+    print('You lost!')
 
 # #stage 4
 # import random
